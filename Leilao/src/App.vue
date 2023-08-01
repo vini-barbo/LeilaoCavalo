fe89cba14ec4e7a8968dce05c3ca111d4f5857bf
Vinicius Barbosa Dos Santos <62946143+vini-barbo@users.noreply.github.com>
2023-07-31 23:25:08 -0300

    Primeiro commit e setup do projeto

---
+++ Leilao/src/App.vue
@@ -0,0 +1,32 @@
+<script >
+import MainSection from './components/body/MainSection.vue';
+import cabecarioMain from './components/cabecario/cabecario.vue';
+
+  export default{
+    components:{
+    cabecarioMain,
+    MainSection
+}
+  }
+
+</script>
+
+<template>
+  <main>
+    <cabecarioMain></cabecarioMain>
+    <MainSection></MainSection>
+  </main>
+</template>
+
+<style>
+*{
+  margin: 0;
+  border: 0;
+  text-decoration: none;
+}
+
+cabecarioMain{
+  margin-bottom: 80px;
+}
+
+</style>
