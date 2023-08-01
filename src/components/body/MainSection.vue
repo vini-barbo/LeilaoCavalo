cb5dc8571706e1d7ee6e4504cfe5dbbd8401231d
Vinicius Barbosa Dos Santos <62946143+vini-barbo@users.noreply.github.com>
2023-07-31 23:42:11 -0300

    Primeiro commit

---
+++ src/components/body/MainSection.vue
@@ -0,0 +1,61 @@
+<script>
+    export default{
+        name:'MainSection'
+    }
+</script>
+
+<template>
+    <main>
+        <div class="img">
+            <img src="./papers/main_paper.jpeg" alt="cavalo"></div>
+        <div class="texto">
+            <span>Destaque</span>
+        </div>
+    </main>
+
+</template>
+
+<style scoped>
+
+main{
+    width: 100%;
+    height: 80vh;
+    display: flex;
+    flex-direction: column;
+
+    overflow-x: hidden;
+  }
+
+div{
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    justify-items: center;    
+}
+
+div.img{
+
+    width: 100%;
+    height: 90%;
+}
+
+img{
+    width: 80%;
+    height: 100%;
+}
+
+div.texto{
+
+    height: 10%;
+    aspect-ratio: 2/1;
+}
+
+span{
+    width: 80%;
+    color: white;
+    font-size: 3rem;
+    background-color: #0369a2;
+    text-align: center;
+}
+
+</style>
---
+++ src/components/cabecario/cabecario.vue
@@ -0,0 +1,67 @@
+<script>
+    export default{
+        name:'cabecarioMain',
+    }
+
+
+</script>
+
+
+<template>
+  <main>
+    <section class="icone">
+        <img src="./icon/logo.svg" alt="Logo cavalo">
+    </section>
+    <section class="links_aside">
+        <div class="link">
+            <a href="#">leilões</a>
+        </div>
+        <div class="link">
+            <a href="#">contato</a>
+            
+        </div>
+        <div class="link">
+            <a href="#">quem somos</a>
+        </div>
+    </section>
+
+  </main>  
+</template>
+
+<style scoped>
+
+main{
+    display: flex;
+    justify-content: space-between;
+    width: 100%;
+}
+
+section{
+    display: flex;
+    height: 90px;
+    width: 100%;
+    font-size: 1.5rem;
+    justify-content: end;
+    padding-left: 10%;
+    padding-right: 10%;
+    align-items: center;
+    gap: 30px;
+    color: #0369a2;
+}
+
+section.icone{
+    justify-content: center;
+}
+
+img{
+    color: #0369a2;
+    width: 70px;
+    aspect-ratio: 1/2;
+}
+
+.link{
+    text-decoration: none;
+}
+
+
+</style>
